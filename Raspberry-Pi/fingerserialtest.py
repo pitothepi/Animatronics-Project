9f0be3eac590755b473c1dfaa57c334b05a5e98b
pitothepi <pitothepi@gmail.com>
2016-11-07 23:20:29 -0500

    Add files via upload

--- Raspberry-Pi/fingerserialtest.py
+++ Raspberry-Pi/fingerserialtest.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as gpio
 import serial
 import time
+import socket
 
+#http://stackoverflow.com/questions/22878625/receiving-broadcast-packets-in-python
 MIN_PWM = 2
 MAX_PWM = 14    #model RS001B servos seem to like this range
 min_bend = 200
@@ -11,9 +13,6 @@ servo_pin = 21
 
 port = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
 port.flush()
-bt = serial.Serial('/dev/rfcomm0', baudrate=115200, timeout=1)
-bt.flush()
-
 
 def wait_for_key():
     trash = input()
@@ -40,7 +39,6 @@ def get_data(pin=0):
     int_val = 0
     for i in range(0, end):
         int_val += int(val[i]) * 10 ** (end - (i + 1))
-    bt.flush()
     return int_val
 
 def get_reading():
@@ -51,25 +49,17 @@ def get_reading():
     avg = sum(readings) / len(readings)
     return int(avg + .5)
 
+sock = socket.socket(AF_INET, SOCK_DGRAM)
+sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
 try:
     while True:
-        #print(get_data())
-        while bt.inWaiting() == 0:
-            time.sleep(.0001)
-        start = time.time();
-        pin = int(bt.read())
-        #data = int(get_data(pin) / 10)
-        #print(str(1/(time.time()-start))+'\thz')
-        datas = []
-        for i in range(0, 5):
-            datas += [int(get_data(i) / 10)]
-        strsend = ""
-        for i in range(0, 4):
-            strsend += str(datas[i]) + ';'
-        strsend += str(datas[-1])
-        bt.write((strsend + ';').encode('utf-8'))
-        #bt.write((str(data)+';').encode('utf-8'))
+        for pin in range(0,5):
+            data = int(get_data(pin) / 10) + pin
+            sock.sendto(str(data).encode('utf-8'), ('255.255.255.255', 1337))
+            time.sleep(.001)
 finally:
-    bt.close()
+    sock.sendto('q'.encode('utf-8'), ('255.255.255.255', 1337))
+    sock.close()
     port.close()
     print('Serial cleanup successful.')
